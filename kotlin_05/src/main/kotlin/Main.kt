ba8ee970873ef3999e00329f9a60a7eaa10d5fd9
Helena Perdigueiro <helena.perdigueiro@gmail.com>
2023-01-03 12:12:57 -0300

    updating kotlin_05

--- kotlin_05/src/main/kotlin/Main.kt
+++ kotlin_05/src/main/kotlin/Main.kt
@@ -83,5 +83,37 @@ fun main(args: Array<String>) {
         println(i)
     }
 
+    val seasons = arrayOf("spring", "summer", "winter", "fall")
+    for (season in seasons) {
+        println(season)
+    }
+    for (index in seasons.indices) {
+        println("${seasons[index]} is season number $index")
+    }
+    seasons.forEach { println(it) }
+    seasons.forEachIndexed { index, value -> println("$value is season $index") }
+
+    val notASeason = "whatever" !in seasons
+    println(notASeason)
+
+    val notInRange = 32 !in 1..10
+    println(notInRange)
+
+    val str2 = "Hello"
+    println('e' in str)
+    println('e' !in str)
+
+    for (i in 1..3) {
+        println("i = $i")
+        jloop@ for (j in 1..3) {
+            println("j = $j")
+            for (k in 1..10) {
+                println("k = $k")
+                if (k == 7) {
+                    break@jloop
+                }
+            }
+        }
+    }
 
 }
